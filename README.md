fc1851fb7a9faa6b1611241d5f0c72081884c329
Eric Mahasi <mahasieric@gmail.com>
2021-10-25 15:24:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# mealmagic
+
+APT3065-A Mid-term Project for Fall Semester 2021. A food ordering app for SMEs.
+
---
+++ android/app/src/main/kotlin/com/eric/mealmagic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eric.mealmagic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
