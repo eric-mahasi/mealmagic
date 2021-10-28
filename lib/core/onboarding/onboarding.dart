43992d70deb93d5efb53f7d0650ec3e5ebbf201a
Eric Mahasi <mahasieric@gmail.com>
2021-10-28 16:49:32 +0300

    Add email sign in and sign up

--- lib/core/onboarding/onboarding.dart
+++ lib/core/onboarding/onboarding.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:mealmagic/core/sign_in/sign_in.dart';
 
 class OnBoardingPage extends StatefulWidget {
   const OnBoardingPage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
 
   void _onIntroEnd(context) {
     Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) => const HomePage()),
+      MaterialPageRoute(builder: (_) => SignInPage()),
     );
   }
 
