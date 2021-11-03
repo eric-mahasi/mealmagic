144761534ecd55d732947abe0699f46717ee4151
Eric Mahasi <mahasieric@gmail.com>
2021-11-03 16:25:53 +0300

    Update color on search widget

--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -6,23 +6,22 @@ class SearchWidget extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.only(left: 10,top: 5,right: 10,bottom: 5),
       child: TextField(
-
         decoration: InputDecoration(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(5.0)),
             borderSide: BorderSide(
               width: 0,
-              color: Color(0xFFfb3132),
+              color: Color(0xfff47a5a),
               style: BorderStyle.none,
             ),
           ),
           filled: true,
           prefixIcon: Icon(
             Icons.search,
-            color: Color(0xFFfb3132),
+            color: Color(0xfff47a5a),
           ),
           fillColor: Color(0xFFFAFAFA),
-          suffixIcon: Icon(Icons.sort,color: Color(0xFFfb3132),),
+          suffixIcon: Icon(Icons.sort,color: Color(0xfff47a5a),),
           hintStyle: new TextStyle(color: Color(0xFFd0cece), fontSize: 18),
           hintText: "Search",
         ),
