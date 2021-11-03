86e592b7f78c4e537dc7b5497c7526baf038015f
Eric Mahasi <mahasieric@gmail.com>
2021-11-03 16:01:29 +0300

    Remove unused home page

--- lib/core/onboarding/onboarding.dart
+++ lib/core/onboarding/onboarding.dart
@@ -118,15 +118,3 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text("This is the screen after Introduction"))
-    );
-  }
-}
